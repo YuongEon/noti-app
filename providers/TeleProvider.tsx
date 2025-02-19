19ebb280fac86d9ebc00a48892cb9c2f10b080c4
YuongEon <boyjackgamer@gmail.com>
2025-02-19 15:41:58 +0700

    feat: import provider

--- providers/TeleProvider.tsx
+++ providers/TeleProvider.tsx
@@ -1,25 +1,33 @@
-import React, { useEffect } from 'react'
+import { useEffect, ReactNode } from 'react'
 import axios from 'axios'
 
-const TeleProvider = () => {
-  const teleToken = process.env.TELEGRAM_BOT_TOKEN
-  const sendMessage = async () => {
-    const response = await axios.get(`https://api.telegram.org/bot${teleToken}/getUpdates`)
-    const chatId =  response.data.result[0].message.chat.id
-    const telegramUrl = `https://api.telegram.org/bot${teleToken}/sendMessage`;
-    await axios.post(telegramUrl, {
-      chat_id: chatId,
-      text: 'Welcom to Noti App',
-    });
-  }
+const TeleProvider = ({ children }: { children: ReactNode }) => {
+	const teleToken = process.env.TELEGRAM_BOT_TOKEN
+	const sendMessage = async () => {
+		const response = await axios.get(
+			`https://api.telegram.org/bot${teleToken}/getUpdates`
+		)
+		const chatId = response.data.result[0].message.chat.id
+		const telegramUrl = `https://api.telegram.org/bot${teleToken}/sendMessage`
 
-  useEffect(() => {
-    sendMessage()
-  }, [])
+		if (!chatId) {
+			await axios.post(telegramUrl, {
+				chat_id: chatId,
+				text: 'Welcom to Noti App',
+			})
+		}
 
-  return (
-    <div>TeleProvider</div>
-  )
+		await axios.post(telegramUrl, {
+			chat_id: chatId,
+			text: 'Welcom back to Noti App',
+		})
+	}
+
+	useEffect(() => {
+		sendMessage()
+	}, [])
+
+	return <>{children}</>
 }
 
 export default TeleProvider
